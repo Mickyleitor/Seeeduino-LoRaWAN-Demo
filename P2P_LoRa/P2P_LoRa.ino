a8526f952c88cee7218969943328d4085b35d61e
Mickyleitor <Mickyleitor@users.noreply.github.com>
2018-04-30 17:21:55 +0200

    First working code

--- P2P_LoRa/P2P_LoRa.ino
+++ P2P_LoRa/P2P_LoRa.ino
@@ -4,16 +4,19 @@
 #define MAX_PAYLOAD 6
 #define ENQ 0x05
 #define ACK 0X06
+#define NAK 0X15
 #define CMD1 0x11
 #define NUM_DEVICES 1
 #define NUM_CMD 1
-#define T_10SEG 10000
-#define T_5SEG 5000
+#define T_10SEG 10
+#define T_5SEG 5
 
 void decode_msg();
-void writeLogMessage(char * text, unsigned char value);
 void writeLogMessage(char * text);
-void blink_t(int times);
+void writeLogMessage(char * text, unsigned char value1);
+void writeLogMessage(char * text, unsigned char value1,unsigned char value2);
+void build_MessageResponseCMD(unsigned char command);
+void build_MessageAskCMD(unsigned char DeviceID,unsigned char command);
 
 uint8_t status = 0;
 uint8_t currentAskingCMD = 0;
@@ -29,17 +32,18 @@ void setup(void)
 {
     SerialUSB.begin(115200);
     randomSeed(analogRead(0));
-    writeLogMessage("Device RESET");
     if(myID == 0) status = 3;
     lora.init();
     lora.initP2PMode(433, SF12, BW125, 8, 8, 20);
+    delay(5000);
+    writeLogMessage("Starting device...");
 }
 
 void loop(void)
 {
-  memset(buffer, 0, 32);
   switch (status){
     case 0 : {
+      memset(buffer, 0, 32);
       if(lora.receivePacketP2PMode(buffer, MAX_PAYLOAD,  &rssi, T_5SEG)){
         decode_msg();
       }
@@ -48,10 +52,15 @@ void loop(void)
     case 1 : {
       switch(buffer[2]){
         case CMD1 : {
-          build_MessageCommandA0();
-          writeLogMessage("Sending the analogical value of A0 [%d] to the requester.",message[4]);
+          build_MessageResponseCMD(CMD1);
+          writeLogMessage("Sending the analogical value of A0 [%d], frame number [%d]",message[5],message[4]);
           lora.transferPacketP2PMode(message,MAX_PAYLOAD,T_5SEG);
           break;
+        }
+        default : {
+          build_MessageResponseCMD(buffer[2]);
+          writeLogMessage("Unknown command resquested, sending a NACK command with fram number [%d]",buffer[4]);
+          lora.transferPacketP2PMode(message,MAX_PAYLOAD,T_5SEG);
         }
       }
       status = 0;
@@ -60,7 +69,7 @@ void loop(void)
     case 2 : {
       switch(buffer[2]){
         case CMD1 : {
-          writeLogMessage("It's analogical value of A0 is %d",buffer[4]);
+          writeLogMessage("The analogical value of A0 from device[%d] is %d",buffer[0],buffer[5]);
           break;
         }
       }
@@ -68,13 +77,24 @@ void loop(void)
       break;
     }
     case 3 : {
-      if(currentAskingID > NUM_DEVICES) currentAskingID = 0;
-      if(currentAskingCMD > NUM_CMD) currentAskingCMD = 0;
-      build_MessageAskCMD();
-      writeLogMessage("Asking the %d device a command",message[1]);
+      memset(buffer, 0, 32);
+      currentAskingID++;
+      if(currentAskingID > NUM_DEVICES){
+        currentAskingID = 1;
+        currentAskingCMD++;
+      }
+      if(currentAskingCMD >= NUM_CMD) currentAskingCMD = 0;
+      build_MessageAskCMD(currentAskingID + myID,currentAskingCMD + CMD1);
+      
+      writeLogMessage("Asking to device[%d] the command[%d]",message[1],message[2]);
+      writeLogMessage("The frame counter is [%d]",message[4]);
       lora.transferPacketP2PMode(message,MAX_PAYLOAD,T_5SEG);
+      memset(buffer, 0, 32);
+      delay(1000);
       if(lora.receivePacketP2PMode(buffer, MAX_PAYLOAD,  &rssi, T_5SEG)){
         decode_msg();
+      }else{
+        writeLogMessage("Time out or no response from device[%d]",currentAskingID + myID);
       }
       break;
     }
@@ -83,14 +103,37 @@ void loop(void)
 
 void decode_msg(){
   if( buffer[1]==myID && buffer[3]==ENQ ) {
-    writeLogMessage("The %d device has asked a command to me.",buffer[0]);
+    writeLogMessage("The device[%d] has asked the command[%d] to me",buffer[0],buffer[2]);
     status = 1;
   }else if( buffer[1]==myID && buffer[3]==ACK ) {
-    writeLogMessage("The %d device has answered the command to me.",buffer[0]);
+    writeLogMessage("The device[%d] has answered the command with frame number [%d] to me",buffer[0],buffer[4]);
     status = 2;
   }else{
-    writeLogMessage("A message from %d device have been ignored.",buffer[0]);
+    writeLogMessage("A message from device[%d] have been ignored.",buffer[0]);
   }
+}
+
+void build_MessageResponseCMD(unsigned char command){
+    message[0]= myID;
+    message[1]= buffer[0];
+    message[2]= command;
+    message[3]= NAK;
+    switch (command) {
+      case CMD1 : {
+          message[3]= ACK;
+          message[4]= buffer[4];
+          message[5]= analogRead(A0);
+      }
+      break;
+    }
+}
+
+void build_MessageAskCMD(unsigned char DeviceID,unsigned char command){
+    message[0]= myID;
+    message[1]= DeviceID;
+    message[2]= command;
+    message[3]= ENQ;
+    message[4]= random(256);
 }
 
 void writeLogMessage(char * text){
@@ -99,36 +142,19 @@ void writeLogMessage(char * text){
     SerialUSB.println(text);
 }
 
-void writeLogMessage(char * text, unsigned char value){
+void writeLogMessage(char * text, unsigned char value1){
     char LogMessages [256];
     memset(LogMessages, 0, 256);
-    sprintf(LogMessages,text, value);
+    sprintf(LogMessages,text, value1);
     SerialUSB.print(myID);
     SerialUSB.print(" : ");
     SerialUSB.println(LogMessages);
 }
-
-void blink_t(int times){
-    int y;
-    for(y = 0; y < times ; y ++ ){
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(250);
-      digitalWrite(LED_BUILTIN, LOW);
-      delay(250);
-  }
-}
-
-void build_MessageCommandA0(){
-    message[0]= myID;
-    message[1]= buffer[0];
-    message[2]= CMD1;
-    message[3]= ACK;
-    message[4]= analogRead(0);
-}
-
-void build_MessageAskCMD(){
-    message[0]= myID;
-    message[1]= myID + currentAskingID;
-    message[2]= CMD1 + currentAskingCMD;
-    message[3]= ENQ;
+void writeLogMessage(char * text, unsigned char value1,unsigned char value2){
+    char LogMessages [256];
+    memset(LogMessages, 0, 256);
+    sprintf(LogMessages,text, value1,value2);
+    SerialUSB.print(myID);
+    SerialUSB.print(" : ");
+    SerialUSB.println(LogMessages);
 }
